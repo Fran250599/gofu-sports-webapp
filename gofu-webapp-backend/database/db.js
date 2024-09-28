208ba593fd10177ff6c2734bf4ed28ada7f605b4
Camilo Gonzalez <camilogonzalezfuentes1303@gmail.com>
2024-09-28 10:45:21 -0600

    Update db.js

--- gofu-webapp-backend/database/db.js
+++ gofu-webapp-backend/database/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb+srv://camilogonzalezfuentes:k98rhIWdCDzN44h9@cluster0.icn3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url = 'mongodb+srv://camilogonzalezfuentes:gofuproyect@cluster0.icn3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(url);
 const connectToDatabase = async () => {
     try {
